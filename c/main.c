49b8abef24061cc7bb009f0c840c3bc3ebd5de24
zhangyue <s20171105132@admindeimac-2.local>
2018-06-21 08:38:03 +0800

    Initial Commit

---
+++ c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c
+//
+//  Created by zhangyue on 18/6/21.
+//  Copyright © 2018年 zhangyue. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
